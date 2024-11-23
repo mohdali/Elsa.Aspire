3bf09ec0f27db2dbb67536de31c49d2d29104899
Mohamed Ali <antivirus.cs@gmail.com>
2024-11-23 10:09:14 +0300

    Add waiting and lifetime

--- Elsa.Aspire.AppHost/Program.cs
+++ Elsa.Aspire.AppHost/Program.cs
@@ -3,18 +3,23 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
-                      .WithDataVolume(isReadOnly: false);
+                      .WithDataVolume(isReadOnly: false)
+                      .WithLifetime(ContainerLifetime.Persistent);
 
 var postgresdb = postgres.AddDatabase("elsadb");
 
-var messaging = builder.AddRabbitMQ("messaging");
+var messaging = builder.AddRabbitMQ("messaging")
+                      .WithLifetime(ContainerLifetime.Persistent);
 
 var server = builder.AddProject<Projects.Elsa_Aspire_Server>("elsaserver")
         .WithReplicas(2)
         .WithReference(postgresdb)
-        .WithReference(messaging);
+        .WithReference(messaging)
+        .WaitFor(postgresdb)
+        .WaitFor(messaging);
 
 builder.AddProject<Projects.Elsa_Aspire_Studio>("elsastudio")
-    .WithReference(server);
+    .WithReference(server)
+    .WaitFor(server);
 
 builder.Build().Run();
